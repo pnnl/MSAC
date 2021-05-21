42f413e02dc695678006da542fa3655d4971cdb9
m-blumer <mraemb@gmail.com>
2021-05-21 13:01:36 -0700

    Add documentation

--- msac/calculate_input_mz.py
+++ msac/calculate_input_mz.py
@@ -1,11 +1,25 @@
+"""
+check : Calculate the combined input+ adduct mass.
+author: @m-blumer
+"""
+# Imports
 import pandas as pd
 import numpy as np
 
-
+# Functions
 def calculate_total_mz(adduct_tuple, mass):
-    '''
-    calculate adduct mz using multiplier, charge, and mass
-    '''
+    """Calculate adduct mz using multiplier, charge, and mass
+    Parameters
+    ----------
+    adduct_tuple : tuple of floats
+        input mass multiplier, adduct charge, and adduct mz 
+    mass : int or float
+        Total mass of input molecule
+    Returns
+    -------
+    float
+        Returns the total mz for the adduct/molecule pair.
+    """
     input_mz_multiplier = adduct_tuple[0]
     adduct_charge = adduct_tuple[1]
     adduct_mz = adduct_tuple[2]
@@ -15,6 +29,21 @@ def calculate_total_mz(adduct_tuple, mass):
 
 
 def calculate_all_mz(df, mass_file, mass_col):
+    """Calculate adduct mz using multiplier, charge, and mass
+    Parameters
+    ----------
+    df : DataFrame
+        Contains an entry representing each adduct of interest, 
+        including adduct name, charge, m/z, and input mass multipler
+    mass_file : str
+        File containing masses of the input molecules.
+    mass_col : str
+        Name of mass column in mass_file.
+    Returns
+    -------
+    DataFrame
+        Returns a table of caluclated masses across all adducts for each input molecule.
+    """
     input_masses = pd.read_csv(mass_file)
 
     #  create a lookup table for the adduct information:
