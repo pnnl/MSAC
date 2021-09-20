1cb8b27455a84c56aa5017000c4ce37f386d7c4d
Blumer <madison.blumer@pnnl.gov>
2021-09-20 09:30:50 -0700

    Adding help text for -n

--- msac/cli.py
+++ msac/cli.py
@@ -13,7 +13,7 @@ def main():
 
     parser.add_argument('input_masses', help=".csv with 'mass' column")
     parser.add_argument('-a', '--adduct_file', help="path to a .csv with an 'adduct' col and a 'charge' col. Defaults to 'example_data/adduct_list.csv' ")
-    parser.add_argument('-n', '--neutral_losses_included', action='store_true')
+    parser.add_argument('-n', '--neutral_losses_included', action='store_true', help="add this flag to use the neutral loss/adduct combination list")
     parser.add_argument('-o', '--outname', help='an output filename (.csv) for the calculated adducts')
     parser.add_argument('-m', '--mass_col', default='mass', type=str, help="if the mass column isn't called 'mass'")
     parser.add_argument('-f', '--formula_col', default=None, type=str, help="specify formula column if you don't have exact mass in your dataframe but have formula")
